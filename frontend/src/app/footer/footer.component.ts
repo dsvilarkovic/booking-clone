10eb46b674a6eca19571fde6bc56583321876aab
Dusan Svilarkovic <tviksi96@gmail.com>
2019-04-16 21:44:33 +0200

    Pocetne komponente, footer i navbar

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mega-travel-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

8463ad082eb4c9a0c39de7e00e12025ca519b401
Boris Jovanovic <bokisjov@gmail.com>
2019-06-07 15:53:43 +0200

    dodato u data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -31,6 +31,14 @@ INSERT INTO public.users (address,email,first_name,last_name,"password",pib,user
 (NULL,'admin@admin.rs',NULL,NULL,'123',NULL,NULL);
 
 
+INSERT INTO public.location(
+	id, address, city, country, latitude, longitude)
+	VALUES (12, 'Bogu iza nogu 4', 'Borkovac na vodi', 'Srbija', 45.036508, 19.820200);
+
 INSERT INTO public.accommodation(
 	id, description, name, accommodation_category_id, accommodation_type_id, location_id, user_id)
-	VALUES (1, 'Neki opis', 'Neko ime', 1, 1, Null, Null);
+	VALUES (1, 'Neki opis', 'Neko ime', 1, 1, 12, 1);
+
+INSERT INTO public.accommodation_unit(
+	id, cancelation_period, capacity, default_price, name)
+	VALUES (12, 7, 4, 120, 'Apartman na vodi');

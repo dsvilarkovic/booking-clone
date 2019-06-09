4db3d67ef4cdce2be0eaccc8a4c4604c068df937
Dusan Svilarkovic <tviksi96@gmail.com>
2019-06-09 05:24:47 +0200

    Added reservation-soap

---
+++ reservation-soap-service/src/main/java/xml/booking/ReservationSoapEndpoint.java
@@ -0,0 +1,112 @@
+package xml.booking;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import xml.booking.model.Reservation;
+import xml.booking.model.User;
+import xml.booking.repositories.ReservationRepository;
+import xml.booking.reservationsoap.CreateReservationRequest;
+import xml.booking.reservationsoap.CreateReservationResponse;
+import xml.booking.reservationsoap.DeclineReservationRequest;
+import xml.booking.reservationsoap.DeclineReservationResponse;
+import xml.booking.reservationsoap.GetReservationMessagesRequest;
+import xml.booking.reservationsoap.GetReservationMessagesResponse;
+import xml.booking.reservationsoap.GetReservationRequest;
+import xml.booking.reservationsoap.GetReservationResponse;
+import xml.booking.reservationsoap.GetReservationsListRequest;
+import xml.booking.reservationsoap.GetReservationsListResponse;
+import xml.booking.reservationsoap.UpdateReservationRequest;
+import xml.booking.reservationsoap.UpdateReservationResponse;
+
+@Endpoint
+public class ReservationSoapEndpoint {
+
+	private static final String NAMESPACE_URI = "http://www.ftn.uns.ac.rs/tim1/reservationsoap";
+
+	@Autowired
+	private ReservationRepository reservationRepository;
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getReservation")
+	@ResponsePayload
+	public GetReservationResponse getReservationRequest(@RequestPayload GetReservationRequest getReservationRequest) {
+		Long reservationId = (long) getReservationRequest.getReservationId();
+		Reservation reservation = reservationRepository.getOne(reservationId);
+		
+		GetReservationResponse getReservationResponse = new GetReservationResponse();
+		
+		getReservationResponse.setReservation(reservation);
+		
+		return getReservationResponse;
+	}
+	
+	/**
+	 * Trazi na osnovu korisnika za koga je rezervisano
+	 * @param getReservationMessagesRequest
+	 * @return
+	 */
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getReservationList")
+	@ResponsePayload
+	public GetReservationsListResponse getReservationsListRequest(@RequestPayload GetReservationsListRequest getReservationMessagesRequest) {
+		GetReservationsListResponse getReservationsListResponse = new GetReservationsListResponse();
+		
+		User user = getReservationMessagesRequest.getUser();
+		
+		List<Reservation> reservation = reservationRepository.findByUserId(user.getId());
+		
+	
+		getReservationsListResponse.setReservation(reservation);
+		
+		return getReservationsListResponse;
+
+	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "createReservation")
+	@ResponsePayload
+	public CreateReservationResponse createReservationResponse(@RequestPayload CreateReservationRequest createReservationRequest) {
+		CreateReservationResponse createReservationResponse = new CreateReservationResponse();
+		Reservation reservation = createReservationRequest.getReservation();
+		reservationRepository.save(reservation);
+		createReservationResponse.setSuccess(true);
+		
+		return createReservationResponse;
+	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "declineReservation")
+	@ResponsePayload
+	public DeclineReservationResponse declineReservation(@RequestPayload DeclineReservationRequest declineReservationRequest) {
+		DeclineReservationResponse declineReservationResponse = new DeclineReservationResponse();
+		
+		Long id = (long) declineReservationRequest.getReservationId();
+		
+		
+		reservationRepository.deleteById(id);
+		
+		declineReservationResponse.setSuccess(true);
+		
+		return declineReservationResponse;
+	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "declineReservation")
+	
+	@ResponsePayload
+	public UpdateReservationResponse updateReservationResponse(@RequestPayload UpdateReservationRequest updateReservationRequest) {
+		UpdateReservationResponse updateReservationResponse = new UpdateReservationResponse();
+		
+		Reservation reservation =  updateReservationRequest.getReservation();
+		
+		reservationRepository.save(reservation);
+		
+		
+		updateReservationResponse.setSuccess(true);
+		
+		return updateReservationResponse;
+	}
+	
+
+}
---
+++ reservation-soap-service/src/main/java/xml/booking/model/AccommodationCategory.java
@@ -0,0 +1,101 @@
+package xml.booking.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="name">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *               &lt;minInclusive value="0"/>
+ *               &lt;maxInclusive value="7"/>
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id",
+    "name",
+    "deleted"
+})
+@XmlRootElement(name = "AccommodationCategory")
+@Entity
+public class AccommodationCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+	
+	@Column(name = "name")
+    protected String name;
+	
+	@Column(name = "deleted")
+	protected boolean deleted;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     */
+    public void setId(long value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+}
